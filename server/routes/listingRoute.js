3b951a0f46212c7f3e2ad65fbdf4b39c91615e9b
Anjay <rajputanjay@gmail.com>
2024-07-08 18:07:47 +0530

    create search API route

--- server/routes/listingRoute.js
+++ server/routes/listingRoute.js
@@ -2,10 +2,11 @@ import { Router } from "express"
 const router = Router();
 
 import verifyJWT from "../utiles/verifyJWT.js";
-import {createListing, deleteListing, updateListing} from "../controllers/listingController.js"
+import {createListing, deleteListing, updateListing, getListing} from "../controllers/listingController.js"
 
 router.route("/listing/create").post(verifyJWT,createListing)
 router.route("/delete-listing/:id").delete(verifyJWT,deleteListing)
 router.route("/update-listing/:id").post(verifyJWT,updateListing)
+router.route('/get').get(getListing)
 
 export default router
