073e6e12a01a342d6a83624217ed1ea8f499821b
Anjay <rajputanjay@gmail.com>
2024-07-10 10:22:41 +0530

    makes some changes in cors

--- server/index.js
+++ server/index.js
@@ -9,12 +9,20 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 // app.use(cors())
-app.use(cors({
-    origin: 'http://localhost:5173',
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
-    credentials: true,
-    allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
-  }));
+// app.use(cors({
+//     origin: 'http://localhost:5173',
+//     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+//     credentials: true,
+//     allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
+//   }));
+
+const cors = require('cors');
+const corsOptions ={
+    origin:'http://localhost:5173', 
+    credentials:true,            
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
 
 app.use(express.json())
 app.use(cookieParser())
