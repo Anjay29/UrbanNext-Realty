1450e525428f58ff570c92f93aeffa7ad82f3d04
Anjay <rajputanjay@gmail.com>
2024-07-10 11:29:03 +0530

    mode some changes inside index.js

--- server/index.js
+++ server/index.js
@@ -9,6 +9,9 @@ import cors from 'cors'
 import dotenv from 'dotenv'
 dotenv.config()
 
+app.use(express.json())
+app.use(cookieParser())
+
 // app.use(cors())
 // app.use(cors({
 //     origin: 'http://localhost:5173',
@@ -23,9 +26,6 @@ const corsOptions ={
     optionSuccessStatus:200
 }
 app.use(cors(corsOptions));
-
-app.use(express.json())
-app.use(cookieParser())
 
 mongoose.connect(process.env.MONGO_URI).then(() => {
     console.log(`Server is running on ${port}`);
