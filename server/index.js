ade5830304aafc12f209e003de17ce83cc631ab0
Anjay <rajputanjay@gmail.com>
2024-07-10 14:21:05 +0530

    made some changes

--- server/index.js
+++ server/index.js
@@ -13,19 +13,19 @@ app.use(express.json())
 app.use(cookieParser())
 
 // app.use(cors())
-// app.use(cors({
-//     origin: 'http://localhost:5173',
-//     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
-//     credentials: true,
-//     allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
-//   }));
+app.use(cors({
+    origin: 'http://localhost:5173',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+    credentials: true,
+    allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
+  }));
 
-const corsOptions ={
-    origin:'http://localhost:5173', 
-    credentials:true,            
-    optionSuccessStatus:200
-}
-app.use(cors(corsOptions));
+// const corsOptions ={
+//     origin:'http://localhost:5173', 
+//     credentials:true,            
+//     optionSuccessStatus:200
+// }
+// app.use(cors(corsOptions));
 
 mongoose.connect(process.env.MONGO_URI).then(() => {
     console.log(`Server is running on ${port}`);
